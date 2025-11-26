911859ee2143eecf6025ada92fc786ac8709c0c6
catlilface <aleklim98@gmail.com>
2025-11-27 00:04:09 +0300

    localStorage fix

--- src/app.tsx
+++ src/app.tsx
@@ -5,18 +5,18 @@ import prompt from "./prompt.md?raw"
 
 let isGenerating = false
 
-if (localStorage.getItem("prompt") === undefined) {
+if (!localStorage.getItem("prompt")) {
     localStorage.setItem("prompt", prompt)
 }
 
-if (localStorage.getItem("key") === undefined) {
+if (!localStorage.getItem("key")) {
     localStorage.setItem("key", window.prompt("Введите ваш ключ OpenRouter. \nДа, это безопасно. Ключ никуда не передается и хранится только на вашем устройстве")!)
 }
 
-if (localStorage.getItem("model") === undefined) {
+if (!localStorage.getItem("model")) {
     localStorage.setItem("model", window.prompt("Введите название модели для генерации. По умолчанию, openai/gpt-oss-20b:free") || "openai/gpt-oss-20b:free")
+    window.location.reload()
 }
-
 
 const App = () => {
     const [content, setContent] = useState("")
